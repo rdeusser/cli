d7a2f20c940c95da2f7a9120eda948a689012ed1
Robert Deusser <robertjdeusser@gmail.com>
2021-02-23 09:50:17 -0500

    chore: remove unused scripts

--- scripts/tag.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-repo_root=$(git rev-parse --show-toplevel)
-version=$(grep -oE "[0-9]+[.][0-9]+[.][0-9]+" "${repo_root}/version/version.go")
-remote=$(git remote -v | awk '{print $1}' | head -n 1)
-
-git tag -a "v${version}" -m "v${version}"
-git push "$remote" "v${version}"`
