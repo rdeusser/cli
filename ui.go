845bf42cacc8c38bb81d5e9b6ec8d0e1e38fde95
Robert Deusser <rdeusser@salesforce.com>
2022-03-07 18:21:34 -0500

    fix: printing output based on output level

--- ui.go
+++ ui.go
@@ -53,29 +53,40 @@ func Output(format string, args ...interface{}) {
 }
 
 func Debug(format string, args ...interface{}) {
-	fmt.Fprintln(output, colorize(ColorYellow, "[DEBUG][%s]:", projectName), fmt.Sprintf(format, args...))
+	if outputLevel.Load() >= int32(DebugLevel) {
+		fmt.Fprintln(output, colorize(ColorYellow, "[DEBUG][%s]:", projectName), fmt.Sprintf(format, args...))
+	}
 }
 
 func Info(format string, args ...interface{}) {
-	fmt.Fprintln(output, colorize(ColorGreen, "[INFO][%s]:", projectName), fmt.Sprintf(format, args...))
+	if outputLevel.Load() >= int32(InfoLevel) {
+		fmt.Fprintln(output, colorize(ColorGreen, "[INFO][%s]:", projectName), fmt.Sprintf(format, args...))
+	}
 }
 
 func Warn(format string, args ...interface{}) {
-	fmt.Fprintln(output, colorize(ColorYellow, "[WARN][%s]:", projectName), fmt.Sprintf(format, args...))
+	if outputLevel.Load() >= int32(WarnLevel) {
+		fmt.Fprintln(output, colorize(ColorYellow, "[WARN][%s]:", projectName), fmt.Sprintf(format, args...))
+	}
 }
 
 func Error(format string, args ...interface{}) {
-	fmt.Fprintln(output, colorize(ColorRed, "[ERROR][%s]:", projectName), fmt.Sprintf(format, args...))
+	if outputLevel.Load() >= int32(ErrorLevel) {
+		fmt.Fprintln(output, colorize(ColorRed, "[ERROR][%s]:", projectName), fmt.Sprintf(format, args...))
+	}
 }
 
 func Fatal(format string, args ...interface{}) {
-	fmt.Fprintln(output, colorize(ColorRed, "[FATAL][%s]:", projectName), fmt.Sprintf(format, args...))
+	if outputLevel.Load() >= int32(FatalLevel) {
+		fmt.Fprintln(output, colorize(ColorRed, "[FATAL][%s]:", projectName), fmt.Sprintf(format, args...))
+	}
 }
 
 func Panic(format string, args ...interface{}) {
-	s := fmt.Sprint(colorize(ColorRed, "[PANIC][%s]:", projectName), fmt.Sprintf(format, args...))
-	panic(s)
-
+	if outputLevel.Load() >= int32(PanicLevel) {
+		s := fmt.Sprint(colorize(ColorRed, "[PANIC][%s]:", projectName), fmt.Sprintf(format, args...))
+		panic(s)
+	}
 }
 
 func colorize(color termenv.Color, format string, args ...interface{}) string {
