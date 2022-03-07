396be8ee0c87012941af95e2f065a73f3bd5d833
Robert Deusser <rdeusser@salesforce.com>
2022-03-07 18:21:40 -0500

    chore: bump version to 0.11.1
    Signed-off-by: Robert Deusser <rdeusser@salesforce.com>

--- version/version.go
+++ version/version.go
@@ -16,7 +16,7 @@ var (
 	//
 	// Version must conform to the format expected by
 	// github.com/hashicorp/go-version for tests to work.
-	Version = "0.11.0"
+	Version = "0.11.1"
 
 	// VersionPrerelease is a pre-release marker for the version. If this is
 	// "" (empty string) then it means that that this is a final
